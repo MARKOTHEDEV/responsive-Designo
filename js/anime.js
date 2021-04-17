328082e946e20f0e6531864578f4259145007f45
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-04-18 07:25:08 +0800

    added animations

---
+++ js/anime.js
@@ -0,0 +1,11 @@
+let animeCover =document.querySelector('.anime-cover');
+
+
+
+
+// animeCover.classList.add('remove-anime')
+
+
+window.addEventListener('load',e=>{
+    animeCover.classList.add('remove-anime')
+})
