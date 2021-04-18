0764b5a944e48167c2cceefe81186777ebecd7d2
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-04-18 08:14:46 +0800

    animation correction

--- js/anime.js
+++ js/anime.js
@@ -7,5 +7,5 @@ let animeCover =document.querySelector('.anime-cover');
 
 
 window.addEventListener('load',e=>{
-    // animeCover.classList.add('remove-anime')
+    animeCover.classList.add('remove-anime')
 })
